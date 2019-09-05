0f0d2b7ead4d9c4e3d0b9493c77809552a281404
Freeman <h398@europa.internal>
2019-09-05 18:38:24 +0100

    Thursday Night Work

--- app/components/client-card.js
+++ app/components/client-card.js
@@ -8,6 +8,7 @@ export default Component.extend({
     showRewardCard: false,
     showErrorMessage: false,
     showVoucherButton: true,
+    loadingVoucher: false,
 
     click() {
         this.set('currentClient', this.item);
@@ -21,14 +22,17 @@ export default Component.extend({
 
             let voucher = this.store.createRecord('voucher', data);
             set(this, 'voucher', voucher);
+            this.set('loadingVoucher', true)
 
             voucher.save().then(() => {
                 this.set('showRewardCard', true)
                 this.set('showErrorMessage', false)
+                this.set('loadingVoucher', false)
             })
                 .catch(() => {
                     this.set('showErrorMessage', true)
                     this.set('showRewardCard', false)
+                    this.set('loadingVoucher', false)
                 });
 
         }
--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,6 +2,8 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
     searchComplete: false,
+    loading: false,
+    noClients: false,
 
     actions: {
         searchClient(query) {
@@ -14,13 +16,15 @@ export default Controller.extend({
 
             let singleClient = this.store.query('client', query);
             this.set('model.clients', singleClient);
+            this.set('loading',true);
             
             singleClient.then(() => {
-                this.set('searchComplete', true);               
+                this.set('searchComplete', true);
+                this.set('loading',false);               
             })
             .catch(() => {
-                // this.set('searchComplete', true)
-
+                this.set('loading',false);   
+                this.set('noClients', true);
             });
 
         },
